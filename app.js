c6df4d5d02254588c73e97fce34e767456f25d56
Dilshod <85446738+dilshodturabov@users.noreply.github.com>
2022-05-23 07:35:59 -0700

    it's too working
    you can generate color in this site

---
+++ app.js
@@ -0,0 +1,10 @@
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function(){
+	let randomColor = Math.floor(Math.random()*(colors.length));
+	document.body.style.backgroundColor = colors[randomColor];
+	color.textContent = colors[randomColor];
+})
+
